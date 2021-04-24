b87369a0c391352acb09aec8fff5d870bb4585b8
Grzegorz (Greg) Twardowski <tvardy@users.noreply.github.com>
2021-04-24 10:05:48 +0000

    rewrite to a es-module with BlobUrl for worker's code
    Closes 78nine/SideWorker#6

---
+++ examples/index.js
@@ -0,0 +1,32 @@
+const w = new SideWorker({
+  debug: true,
+  init: (value = 11) => {
+    self.value = value
+    console.debug(`:: init(${value})`)
+  }
+}, 21)
+
+w.define(
+  'ask',
+  (num = 1) => {
+    console.debug(`:: ask(${num})`)
+    return num * self.value
+  },
+  res => console.log(`the 'ask' result is: ${res}`)
+  )
+
+  w.ask(2)
+
+  w.define(
+    'check',
+    (name) => {
+      console.debug(`:: check('${name}')`)
+      return { name, exists: !!worker[name] && typeof worker[name] === 'function' }
+    },
+  ({ name, exists }) => console.log(`worker.${name}() ${exists ? 'exists' : 'does NOT exist'}`)
+)
+
+w.check('loadLibrary')
+w.check('ask')
+w.check('check')
+w.check('WHAT_EVER')
